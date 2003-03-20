76f22588f0ecbaaf694cc2ef0bad5a92643c1c87
Norman Walsh <ndw@nwalsh.com>
2003-03-20 21:16:46 +0000

    Updated paths for Xerces

--- cvstools/common-functions.sh
+++ cvstools/common-functions.sh
@@ -1,11 +1,13 @@
-findxerces1 () {
+findxerces2 () {
+  # xerces2, which requires two jars, xercesImpl.jar and xmlParserAPIs.jar
   if [ ! "$XERCES" ]; then
-    for path in "/projects/apache/xml-xerces/java/build/classes" \
-                "/usr/local/share/java/xerces.jar" \
-                "/usr/local/java/xerces.jar" \
-                "/usr/share/java/xerces.jar"; do
-      if [ -f "$path" -o -d "$path" ]; then
-        XERCES="$path"
+    for path in "/usr/local/share/java" \
+                "/usr/local/java/xerces2" \
+                "/usr/local/java" \
+                "/usr/share/java"; do
+      if [ -d "$path" -a -f "$path/xercesImpl.jar" -a \
+	 		 -f "$path/xmlParserAPIs.jar" ]; then
+        XERCES="$path/xercesImpl.jar:$path/xmlParserAPIs.jar"
         break
       fi
     done
@@ -16,8 +18,7 @@ findxerces1 () {
 findcrimson () {
   # crimson, an older Java XML parser
   if [ ! "$CRIMSON" ]; then
-    for path in "/projects/apache/xml-crimson/java/build/classes" \
-                "/usr/local/share/java/crimson.jar" \
+    for path in "/usr/local/share/java/crimson.jar" \
                 "/usr/local/java/crimson.jar" \
                 "/usr/share/java/crimson.jar"; do
       if [ -f "$path" -o -d "$path" ]; then
@@ -29,15 +30,14 @@ findcrimson () {
   echo $CRIMSON
 }
 
-findxerces2 () {
-  # xerces2, which requires two jars, xercesImpl.jar and xmlParserAPIs.jar
+findxerces1 () {
   if [ ! "$XERCES" ]; then
-    for path in "/usr/local/share/java" \
-                "/usr/local/java" \
-                "/usr/share/java"; do
-      if [ -d "$path" -a -f "$path/xercesImpl.jar" -a \
-	 		 -f "$path/xmlParserAPIs.jar" ]; then
-        XERCES="$path/xercesImpl.jar:$path/xmlParserAPIs.jar"
+    for path in "/usr/local/share/java/xerces.jar" \
+                "/usr/local/java/xerces1/xerces.jar" \
+                "/usr/local/java/xerces.jar" \
+                "/usr/share/java/xerces.jar"; do
+      if [ -f "$path" -o -d "$path" ]; then
+        XERCES="$path"
         break
       fi
     done
