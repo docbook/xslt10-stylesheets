94b02af652ae5f38d53dd81fd2339ece9ce839b7
Michael Smith <xmldoc@users.sourceforge.net>
2007-07-23 08:42:24 +0000

    Added various changes to automate parts of the release build that hadn't yet been automated, and to better integrate the docbook-xsl-ns upload/install into the release build:
      - we now build a "partial" text version of the release notes,
        RELEASE-NOTES-PARTIAL.text, to use in the release
        announcements and SF release notes
    
      - now use a copy of the freshmeat-submit script in the
        releasetools dir (so that others don't have to separately
        download and install it)
    
      - added an "announce" target that calls the "release-announce"
        script, which sends announcement mail to docbook-apps

--- releasetools/xslns.mk
+++ releasetools/xslns.mk
@@ -4,15 +4,14 @@
 
 # $Id$
 
-debug:
-
 freshmeat-ns:
 ifeq ($(SFRELID),)
 	@echo "You must specify the sourceforge release identifier in SFRELID"
 	exit 1
 else
-	$(XSLT) VERSION VERSION $(TMP)/fm-docbook-$(DISTRO)-ns sf-relid=$(SFRELID)
-	grep -v "<?xml" $(TMP)/fm-docbook-$(DISTRO)-ns | freshmeat-submit $(FMGO)
+	$(XSLT) $(TMP)/docbook-$(DISTRO)-ns-$(ZIPVER)/VERSION $(TMP)/docbook-$(DISTRO)-ns-$(ZIPVER)/VERSION \
+	  $(TMP)/fm-docbook-$(DISTRO)-ns sf-relid=$(SFRELID)
+	grep -v "<?xml" $(TMP)/fm-docbook-$(DISTRO)-ns | $(FRESHMEAT_SUBMIT) $(FMGO)
 endif
 
 zip-ns: zip
@@ -28,7 +27,7 @@ zip-ns: zip
 	  docbook-$(DISTRO)-ns-$(ZIPVER)); 
 
 # change branch info
-	sed -i "s/^\(.*\)<fm:Branch>XSL Stylesheets<\/fm:Branch>$$/\1<fm:Branch>XSL NS Stylesheets<\/fm:Branch>/" \
+	sed -i "s/^\(.*\)<fm:Branch>XSL Stylesheets<\/fm:Branch>$$/\1<fm:Branch>XSL-NS Stylesheets<\/fm:Branch>/" \
 	  $(TMP)/docbook-$(DISTRO)-ns-$(ZIPVER)/VERSION
 
 # change distro name
@@ -47,22 +46,49 @@ zip-ns: zip
 	umask 022; cd $(TMP) && $(ZIP) $(ZIPFLAGS) docbook-$(DISTRO)-ns-$(ZIPVER).zip \
 	 docbook-$(DISTRO)-ns-$(ZIPVER)
 
-install-ns: zip-ns install
+upload-to-project-webspace-ns: zip-ns
 ifeq ($(SF_USERNAME),)
 	$(error You must specify a value for $$SF_USERNAME)
 else
-	-$(FTP) $(FTP_OPTS) "mput -O $(SF_UPLOAD_DIR) $(TMP)/docbook-$(DISTRO)-ns-$(ZIPVER).*; quit" $(SF_UPLOAD_HOST)
+	-$(SCP) $(SCP_OPTS) $(TMP)/docbook-$(DISTRO)-$(ZIPVER).tar.bz2 $(SF_USERNAME)@$(PROJECT_HOST):$(RELEASE_DIR)/$(DISTRO)/
+	-$(SCP) $(SCP_OPTS) $(TMP)/docbook-$(DISTRO)-doc-$(ZIPVER).tar.bz2 $(SF_USERNAME)@$(PROJECT_HOST):$(RELEASE_DIR)/$(DISTRO)/
 	-$(SCP) $(SCP_OPTS) $(TMP)/docbook-$(DISTRO)-ns-$(ZIPVER).tar.bz2 $(SF_USERNAME)@$(PROJECT_HOST):$(RELEASE_DIR)/$(DISTRO)-ns/
 	-$(SSH) $(SSH_OPTS)-l $(SF_USERNAME) $(PROJECT_HOST) \
 	  "(\
 	   umask 002; \
 	   cd $(RELEASE_DIR)/$(DISTRO)-ns; \
 	   rm -rf $(ZIPVER); \
+	   cp -p ../$(DISTRO)/docbook-$(DISTRO)-doc-$(ZIPVER).tar.bz2 .; \
 	   $(TAR) xfj$(TARFLAGS) docbook-$(DISTRO)-ns-$(ZIPVER).tar.bz2; \
+	   $(TAR) xfj$(TARFLAGS) docbook-$(DISTRO)-doc-$(ZIPVER).tar.bz2; \
 	   mv docbook-$(DISTRO)-ns-$(ZIPVER) $(ZIPVER); \
-	   rm -rf docbook-$(DISTRO)-ns-$(ZIPVER).tar.bz2; \
+	   mv docbook-$(DISTRO)-$(ZIPVER)/doc $(ZIPVER); \
+	   rm -rf docbook-$(DISTRO)-$(ZIPVER); \
+	   gunzip $(ZIPVER)/doc/reference.pdf.gz; \
+	   gunzip $(ZIPVER)/doc/reference.txt.gz; \
 	   chmod -R g+w $(ZIPVER); \
 	   $(RM) current; \
 	   ln -s $(ZIPVER) current; \
+	   rm -f docbook-$(DISTRO)-ns-$(ZIPVER).tar.bz2; \
+	   rm -f docbook-$(DISTRO)-doc-$(ZIPVER).tar.bz2; \
+	   cd $(RELEASE_DIR)/$(DISTRO); \
+	   rm -rf $(ZIPVER); \
+	   $(TAR) xfj$(TARFLAGS) docbook-$(DISTRO)-$(ZIPVER).tar.bz2; \
+	   $(TAR) xfj$(TARFLAGS) docbook-$(DISTRO)-doc-$(ZIPVER).tar.bz2; \
+	   mv docbook-$(DISTRO)-$(ZIPVER) $(ZIPVER); \
+	   gunzip $(ZIPVER)/doc/reference.pdf.gz; \
+	   gunzip $(ZIPVER)/doc/reference.txt.gz; \
+	   chmod -R g+w $(ZIPVER); \
+	   $(RM) current; \
+	   ln -s $(ZIPVER) current; \
+	   rm -f docbook-$(DISTRO)-$(ZIPVER).tar.bz2; \
+	   rm -f docbook-$(DISTRO)-doc-$(ZIPVER).tar.bz2; \
 	   )"
 endif
+
+install-ns: upload-to-sf-incoming upload-to-project-webspace-ns
+	@echo "The docbook-$(DISTRO), docbook-$(DISTRO)-ns, docbook-$(DISTRO)-ns packages have been uploaded to"
+	@echo "the SF incoming area."
+	@echo "Use the following form to move the uploaded files to the project release area."
+	@echo
+	@echo "  http://sourceforge.net/project/admin/editpackages.php?group_id=21935"
