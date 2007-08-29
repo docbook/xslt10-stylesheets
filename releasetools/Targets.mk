043f14a4c3c5fa9de95af1f956c2a42869697d95
Michael Smith <xmldoc@users.sourceforge.net>
2007-08-29 09:51:55 +0000

    Created INSTALL_DEPENDS variable, for the install target.

--- releasetools/Targets.mk
+++ releasetools/Targets.mk
@@ -221,7 +221,7 @@ else
 	   )"
 endif
 
-install: release tag upload-to-sf-incoming upload-to-project-webspace
+install: $(INSTALL_DEPENDS) upload-to-sf-incoming upload-to-project-webspace
 	@echo "The docbook-$(DISTRO) and docbook-$(DISTRO)-doc packages have been uploaded to the SF incoming area."
 	@echo "Use the form at the following URL to move files to the project release area."
 	@echo
